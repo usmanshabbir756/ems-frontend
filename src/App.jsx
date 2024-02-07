fa61dd8264ad9e3cb479d484a973933259df6c4c
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-07 19:39:50 +0500

    Routes configrations

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeesComponent from './components/ListEmployeesComponent'
+import { BrowserRouter , Routes,Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent />
-      <ListEmployeesComponent />
+      <Routes>
+        {/* // http:localhost:3000 */}
+        <Route path='/' element={<ListEmployeesComponent/>}></Route>
+        {/* // http:localhost:3000/employees */}
+        <Route path='/employees' element={<ListEmployeesComponent/>}></Route>
+      </Routes>
       <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
