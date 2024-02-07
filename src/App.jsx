036d9c40c0473c889eacf9138fe34769928c4fe8
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-07 18:37:46 +0500

    List of Employees using rest api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ListEmployeesComponent from './components/ListEmployeesComponent'
 
 function App() {
 
   return (
     <>
-     <h1 className='text-start'>Hello World!</h1>
+     <ListEmployeesComponent />
     </>
   )
 }
---
+++ src/components/ListEmployeesComponent.jsx
@@ -0,0 +1,41 @@
+import React , {useState , useEffect} from 'react'
+import { listEmployees } from '../services/ListEmployeesService';
+
+const ListEmployeesComponent = () => {
+
+    const [employees,setEpmloyees] = useState([]);
+
+    useEffect(()=>{
+        listEmployees().then((response)=> setEpmloyees(response.data)).catch(error=>console.error(error));
+    },[])
+
+  return (
+    <div className='container'>
+        <h2>List Of Employees</h2>
+        <table className='table table-striped table-bordered text-start'>
+            <thead>
+                <tr>
+                    <th>Employee ID</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    employees.map((employee)=>
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListEmployeesComponent
